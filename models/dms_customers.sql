d5541637397eedb26466194d5048c6d5c618e110
admin@markethardware.com <admin@markethardware.com>
2021-08-11 16:32:27 +0000

    2

--- models/dms_customers.sql
+++ models/dms_customers.sql
@@ -1,5 +1,3 @@
-with dms_customers as (
     select
         id as accountid, 'BV' as subsidiary, organization as name, first_name as fname, last_name as lname, email, phone, address as street1, address_2 as street2, city, state, country, zip as postalcode, created_at as createdt, updated_at as lastmoddt 
     from brighter_vision_monthly_transactions.bv_customers
-)
